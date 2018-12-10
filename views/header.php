cf231f387a1fd5402f7131a58121c085b0db733a
Isabelle Krall <isabellekrall@gmail.com>
2018-12-10 12:49:02 +0100

    added header.php

---
+++ views/header.php
@@ -0,0 +1,14 @@
+<!-- add the default application setup here: -->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    Hello
+
+  </body>
+</html>
