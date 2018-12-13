97a90bb85e02ebd19ad987ab5a48130b05f61ecc
Isabelle Krall <isabellekrall@gmail.com>
2018-12-13 11:40:36 +0100

    updated header.php

--- views/header.php
+++ views/header.php
@@ -1,14 +1,20 @@
-<!-- add the default application setup here: -->
 
+<?php
+// Always start by loading the default application setup.
+require __DIR__.'/../app/autoload.php';
+?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
+<html lang="en">
+<head>
     <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    Hello
+    <title>Photoify</title>
 
-  </body>
-</html>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/styles/main.css">
+</head>
+<body>
+    <?php require __DIR__.'/navigation.php'; ?>
+
+    <div class="container py-5">
