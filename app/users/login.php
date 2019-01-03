23a2c37a459be6ec4f7a7fd690aa309533cfae55
Isabelle Krall <isabellekrall@gmail.com>
2019-01-03 11:41:23 +0100

    cleaned up the code

--- app/users/login.php
+++ app/users/login.php
@@ -8,7 +8,7 @@ require __DIR__.'/../autoload.php';
 if (isset($_POST['email'], $_POST['password'])) {
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    // Prepare, bind email parameter and execute the database query.
+    //Prepare, bind email parameter and execute the database query.
     $statement = $pdo->prepare('SELECT * FROM Users WHERE email = :email');
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->execute();
@@ -16,24 +16,16 @@ if (isset($_POST['email'], $_POST['password'])) {
     // Fetch the user as an associative array.
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
-    // If we couldn't find the user in the database, redirect back to the login
-    // page with our custom redirect function.
+    // If we couldn't find the user in the database, redirect back to the login page with our custom redirect function.
     if (!$user) {
         redirect('/login.php');
     }
 
-    // If we found the user in the database, compare the given password from the
-    // request with the one in the database using the password_verify function.
+    //Compare the given password.
     if (password_verify($_POST['password'], $user['password'])) {
-        // If the password was valid we know that the user exists and provided
-        // the correct password. We can now save the user in our session.
-        // Remember to not save the password in the session!
         unset($user['password']);
-
         $_SESSION['user'] = $user;
     }
 }
 
-// We should put this redirect in the end of this file since we always want to
-// redirect the user back from this file. We don't know
 redirect('/');
