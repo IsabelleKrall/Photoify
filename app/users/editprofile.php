05b3376fc04805bed0765ae9bccc557ca72b02e8
Isabelle Krall <isabellekrall@gmail.com>
2019-01-03 11:50:52 +0100

    corrected spelling

--- app/users/editprofile.php
+++ app/users/editprofile.php
@@ -14,7 +14,7 @@ if (isset($_POST['profile_bio'], $_POST['name'], $_POST['email'], $_POST['userna
       $bio = trim(filter_var($_POST['profile_bio'],FILTER_SANITIZE_STRING));
       $name = trim(filter_var($_POST['name'], FILTER_SANITIZE_STRING));
       $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
-      $userName = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
+      $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
       $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     }
 
@@ -29,7 +29,7 @@ if (isset($_POST['profile_bio'], $_POST['name'], $_POST['email'], $_POST['userna
     $statement->bindParam(':profile_bio', $bio, PDO::PARAM_STR);
     $statement->bindParam(':name', $name, PDO::PARAM_STR);
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
-    $statement->bindParam(':username', $userName, PDO::PARAM_STR);
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
     $statement->bindParam(':password', $password, PDO::PARAM_STR);
     $statement->bindParam(':id', $id, PDO::PARAM_INT);
     $statement->execute();
