4b567fe01df5fd64af6b21abcfce5f9dce16abe7
Isabelle Krall <isabellekrall@gmail.com>
2019-01-03 11:42:39 +0100

    added insert $statement

--- app/users/editprofile.php
+++ app/users/editprofile.php
@@ -19,10 +19,33 @@ if (isset($_POST['profile_bio'], $_POST['name'], $_POST['email'], $_POST['userna
     }
 
     $statement = $pdo->prepare('UPDATE Users SET profile_bio = :profile_bio, name = :name, email = :email, username = :username, password = :password
-      WHERE id = :id');
-      
+    WHERE id = :id');
+
         if (!$statement){
           die(var_dump($pdo->errorInfo()));
         }
 
-}
+    //Binds variables to parameteres for insert statement
+    $statement->bindParam(':profile_bio', $bio, PDO::PARAM_STR);
+    $statement->bindParam(':name', $name, PDO::PARAM_STR);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':username', $userName, PDO::PARAM_STR);
+    $statement->bindParam(':password', $password, PDO::PARAM_STR);
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+
+    $statement = $pdo->prepare('SELECT * FROM Users WHERE id = :id');
+    $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_STR);
+    $statement->execute();
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['logedin'] = [
+        'id' => $user['id'],
+        'email' => $user['email'],
+        'name' => $user['name'],
+        'profile_pic' => $user['profile_pic'],
+        'profile_bio' => $user['profile_bio'],
+        'username' => $user['username'],
+        ];
+
+    redirect('/profile.php');
+    }
