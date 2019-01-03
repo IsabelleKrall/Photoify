d6ac75379e39c37b70d5b186b8113ac99b0ae1b3
Isabelle Krall <isabellekrall@gmail.com>
2019-01-03 13:01:09 +0100

    addde firstName, lastName to user data

--- app/users/editprofile.php
+++ app/users/editprofile.php
@@ -5,34 +5,38 @@ declare(strict_types=1);
 require __DIR__.'/../autoload.php';
 
 
-//Checks if exists in the POST request:
-if (isset($_POST['profile_bio'], $_POST['name'], $_POST['email'], $_POST['username'],$_POST['password']){
-  if(!isset($_SESSION['user'])){
-    redirect('/profile.php')
-  } else {
-      $id = $_SESSION['user']['id'];
+//Checks if user data exists in the POST request:
+if (isset($_POST['profile_bio'], $_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['username'],$_POST['password'], $_POST['confirmPassword']))
+  {
+   //Checks if passwords match:
+   if ($_POST['password'] === $_POST['confirmPassword'])
+   {
+    //Sanitizes and saves posted user data to variables
       $bio = trim(filter_var($_POST['profile_bio'],FILTER_SANITIZE_STRING));
-      $name = trim(filter_var($_POST['name'], FILTER_SANITIZE_STRING));
-      $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
-      $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
+      $firstName = filter_var(trim($_POST['firstName']), FILTER_SANITIZE_STRING);
+      $lastName = filter_var(trim($_POST['lastName']), FILTER_SANITIZE_STRING);
+      $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+      $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
       $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    }
+      $id = $_SESSION['user']['id'];
 
-    $statement = $pdo->prepare('UPDATE Users SET profile_bio = :profile_bio, name = :name, email = :email, username = :username, password = :password
-    WHERE id = :id');
+      $statement = $pdo->prepare('UPDATE Users SET profile_bio = :profile_bio, first_name = :firstName, last_name = :lastName, email = :email, username = :username, password = :password WHERE id = :id');
 
         if (!$statement){
           die(var_dump($pdo->errorInfo()));
         }
 
-    //Binds variables to parameteres for insert statement
-    $statement->bindParam(':profile_bio', $bio, PDO::PARAM_STR);
-    $statement->bindParam(':name', $name, PDO::PARAM_STR);
-    $statement->bindParam(':email', $email, PDO::PARAM_STR);
-    $statement->bindParam(':username', $username, PDO::PARAM_STR);
-    $statement->bindParam(':password', $password, PDO::PARAM_STR);
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->execute();
+      //Binds variables to parameteres for insert statement:
+        $statement->bindParam(':profile_bio', $profile_bio, PDO::PARAM_STR);
+        $statement->bindParam(':firstName', $firstName, PDO::PARAM_STR);
+        $statement->bindParam(':lastName', $lastName, PDO::PARAM_STR);
+        $statement->bindParam(':email', $email, PDO::PARAM_STR);
+        $statement->bindParam(':username', $username, PDO::PARAM_STR);
+        $statement->bindParam(':password', $password, PDO::PARAM_STR);
+        $statement->bindParam(':id', $id, PDO::PARAM_STR);
+        $statement->execute();
+      }
+
 
     $statement = $pdo->prepare('SELECT * FROM Users WHERE id = :id');
     $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_STR);
@@ -41,11 +45,12 @@ if (isset($_POST['profile_bio'], $_POST['name'], $_POST['email'], $_POST['userna
     $_SESSION['logedin'] = [
         'id' => $user['id'],
         'email' => $user['email'],
-        'name' => $user['name'],
+        'firstName' => $user['firstName'],
+        'lastName' => $user['lastName'],
         'profile_pic' => $user['profile_pic'],
         'profile_bio' => $user['profile_bio'],
         'username' => $user['username'],
         ];
 
     redirect('/profile.php');
-    }
+  };
