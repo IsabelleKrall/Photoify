df956f13cdf6a3bb6d68247ca0dcf56b11468e33
Isabelle Krall <isabellekrall@gmail.com>
2019-01-05 00:00:23 +0100

    updated styling

--- login.php
+++ login.php
@@ -11,54 +11,63 @@
         <div class="form-group">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" id="password" required>
-            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+            <small class="form-text text-muted">Please provide your password (passphrase).</small>
         </div><!-- /form-group -->
 
         <button type="submit" class="btn btn-primary">Log in</button>
   </form>
 </br>
 </br>
-<h3>Create account</h3>
 
+<h3>Don't have an account? <a href="/signup.php">Sign up</a></h3>
+
+<!-- KEEP REGISTER ON THE SAME PAGE AS LOG IN?? IF YES USE THIS BELOW: -->
+<!-- <h3>Create account</h3>
 <form action="app/users/register.php" method="post">
         <div class="form-group">
             <label for="email">Email</label>
             <input class="form-control" type="email" name="email" id="email" required>
             <small class="form-text text-muted">Please provide your email address.</small>
-        </div><!-- /form-group -->
+        </div> -->
+        <!-- /form-group -->
 
-        <div class="form-group">
+        <!-- <div class="form-group">
             <label for="username">Username</label>
             <input class="form-control" type="username" name="username" id="username" required>
             <small class="form-text text-muted">Please provide your username.</small>
-        </div><!-- /form-group -->
+        </div> -->
+        <!-- /form-group -->
 
-        <div class="form-group">
+        <!-- <div class="form-group">
             <label for="firstName">First name</label>
             <input class="form-control" type="name" name="firstName" id="firstName" required>
             <small class="form-text text-muted">Please provide your first name.</small>
-        </div><!-- /form-group -->
+        </div> -->
+        <!-- /form-group -->
 
-        <div class="form-group">
+        <!-- <div class="form-group">
             <label for="lastName">Last name</label>
             <input class="form-control" type="name" name="lastName" id="lastName" required>
             <small class="form-text text-muted">Please provide your last name.</small>
-        </div><!-- /form-group -->
+        </div> -->
+        <!-- /form-group -->
 
-        <div class="form-group">
+        <!-- <div class="form-group">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" id="password" required>
             <small class="form-text text-muted">Please provide your password.</small>
-        </div><!-- /form-group -->
+        </div> -->
+        <!-- /form-group -->
 
-        <div class="form-group">
+        <!-- <div class="form-group">
             <label for="password">Confirm Password</label>
             <input class="form-control" type="password" name="confirmPassword" id="confirmPassword" required>
             <small class="form-text text-muted">Please confirm your password.</small>
-        </div><!-- /form-group -->
+        </div> -->
+        <!-- /form-group -->
 
-        <button type="submit" name ="submit" class="btn btn-primary">Create account</button>
-    </form>
+        <!-- <button type="submit" name ="submit" class="btn btn-primary">Create account</button>
+    </form> -->
 
 
 <?php require __DIR__.'/views/footer.php'; ?>
--- profile.php
+++ profile.php
@@ -3,31 +3,59 @@ declare(strict_types=1);
 require __DIR__.'/views/header.php';
 ?>
 
-<article>
-  <?php if (isset($_SESSION['user'])): ?>
-      <p>Welcome, <?php echo $_SESSION['user']['name']; ?>!</p>
-  <?php endif; ?>
-</article>
 
 
-<!-- //To-do: add statement if pp exist - show pp. -->
+<div class="profile-container">
 
-
-<form action="/views/profile.php" method="post" enctype="multipart/form-data">
-    <div>
-        <label for="image">Choose a profile picture</label>
-        <input type="file" name="profile_pic" id="image" accept=".jpg", ".jpeg", ".png" required>
+    <div class="profile-image-container">
+         <img class="profile-image" src="/views/img/<?= $_SESSION['logedin']['profile_pic'];?>" alt="">
     </div>
 
-    <button type="submit" name ="submit">Upload picture</button>
-</form>
+    <form action="/views/profile.php" method="post" enctype="multipart/form-data">
+        <div>
+            <label for="image">Choose a profile picture</label>
+            <input type="file" name="profile_pic" id="image" accept=".jpg", ".jpeg", ".png" required>
+        </div>
 
-<div class="biography">
+        <button type="submit" name ="submit">Upload picture</button>
+    </form>
+
+    <h1> <?=$_SESSION['user']['username'];?></h1>
+
+
+  <div class="user-profile">
+        <a href="update-user.php">
+          <p class="profile-edit">Edit profile</p>
+        </a>
+        <a href="/app/users/logout.php">
+            <p class="profile-edit">Logout</p>
+        </a>
+  </div>
+
+</div>
+
+
+
+
+<!-- <div class="profile-content">
+  <a href="/app/users/editprofile.php">
+    <p class="edit-profile">Edit profile</p></a>
+  <a href="/password_update.php">
+    <p class="change-password"> Change password</p></a>
+  <a href="/app/users/logout.php">
+    <p class="log-out">Log out</p></a>
+</div> -->
+
+
+</div>
+
+
+<div class="bio">
     <form action="/app/users/biography.php" method="post" enctype="multipart/form-data">
         <div class="bio-form">
             <p>Name:</p> <!--add echo statement here-->
             <p>Username:</p> <!--add echo statement here-->
-            <p>Biography:</p> <!--add echo statement here-->
+            <p>Biography:</p> <!--add echo statement here--> <?php echo $_SESSION['logedin']['profile_bio']; ?></p>
             <textarea class="bio-text" type="user_bio" name="user_bio" id="user_bio"></textarea>
         </div>
     </div>
