ee7d0f6359537a5fcc2af1e3e4ef5889d7b7d8eb
Isabelle Krall <isabellekrall@gmail.com>
2019-01-05 00:02:34 +0100

    updated user session + some content

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<article>
-    <h1>Photoify</h1>
-    <p>This is the home page.</p>
+<h1>Photoify</h1>
 
-    <?php if (isset($_SESSION['user'])): ?>
-        <p>Welcome, <?php echo $_SESSION['user']['name']; ?>!</p>
-    <?php endif; ?>
-</article>
+<?php if (isset($_SESSION['user'])): ?>
+      <p>Welcome, <?php echo $_SESSION['user']['username']; ?>!</p>
+<?php endif; ?>
+
+<?php if (!isset($_SESSION['user'])): ?>
+<h3>Sign up to see photos from your friends
+<a class="" href="/signup.php">Sign up</a></h4>
+
+
+
+  <h4>Have an account? <a class="" href="/login.php">Log in</a></h4>
+
+
+    <?php endif;?>
+
+
+<!-- Redirect to login if user is not logged in: -->
+
+
+
 
 <?php require __DIR__.'/views/footer.php'; ?>
