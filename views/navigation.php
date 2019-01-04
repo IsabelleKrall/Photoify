a1202ce8524c9337284f54b4568daf96a78d300c
Isabelle Krall <isabellekrall@gmail.com>
2019-01-05 00:00:50 +0100

    updated user session

--- views/navigation.php
+++ views/navigation.php
@@ -2,28 +2,24 @@
     <a class="navbar-brand" href="#">Photoify</a>
 
     <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
+      </li><!-- /nav-item -->
+      <?php if (!isset($_SESSION['user'])): ?>
         <li class="nav-item">
-          <?php if(isset($_SESSION['user'])): ?>
-            <a class="nav-link" href="/profile.php">Profile</a>
-          <?php else: ?>
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
-          <?php endif; ?>
+          <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/signup.php' ? 'active' : ''; ?>" href="/signup.php">Sign up</a>
         </li><!-- /nav-item -->
+      <?php endif;?>
 
-        <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/about.php' ? 'active' : ''; ?>" href="/about.php">About</a>
-        </li><!-- /nav-item -->
-
-        <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/signup.php' ? 'active' : ''; ?>" href="/signup.php">Signup</a>
-        </li><!-- /nav-item -->
-
-        <li class="nav-item">
-            <?php if (isset($_SESSION['user'])): ?>
-                <a class="nav-link" href="/app/users/logout.php">Logout</a>
-            <?php else: ?>
-                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
-            <?php endif; ?>
-        </li><!-- /nav-item -->
-    </ul><!-- /navbar-nav -->
+      <li class="nav-item">
+    <?php if (isset($_SESSION['user'])): ?>
+      </li><!-- /nav-item -->
+        <a class="nav-link" href="/profile.php">Profile</a>
+        <a class="nav-link" href="/update-user.php">Update</a>
+        <a class="nav-link" href="/app/users/logout.php">Logout</a>
+    <?php else: ?>
+        <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+    <?php endif; ?>
+</li><!-- /nav-item -->
+</ul><!-- /navbar-nav -->
 </nav><!-- /navbar -->
