0dd5dbf24302da32eae1deaceca6da4b4f1e1327
Isabelle Krall <isabellekrall@gmail.com>
2019-01-04 23:58:32 +0100

    check email & password

--- app/users/login.php
+++ app/users/login.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
+
 //Checks if both email and password exists in the POST request.
 if (isset($_POST['email'], $_POST['password'])) {
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
@@ -15,6 +16,9 @@ if (isset($_POST['email'], $_POST['password'])) {
 
     //Fetch the user as an associative array.
     $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+
+
 
     //If we dont find the user in db, redirect back to the login page.
     if (!$user) {
