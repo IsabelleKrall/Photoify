af69bd87cc845ad2250790f17ee11332fc910601
Isabelle Krall <isabellekrall@gmail.com>
2019-01-15 16:27:56 +0100

    cleaned up the code

--- app/users/register.php
+++ app/users/register.php
@@ -4,7 +4,8 @@ require __DIR__.'/../autoload.php';
 
 
 // //Checks if user data exists in the POST request:
-if(isset($_POST['email'], $_POST['password'], $_POST['username'], $_POST['firstName'], $_POST['lastName'], $_POST['password'], $_POST['confirmPassword'])) {
+if(isset($_POST['email'], $_POST['password'], $_POST['username'], $_POST['firstName'], $_POST['lastName'], $_POST['password'], $_POST['confirmPassword']))
+{
 
   //Checks if the password match & sanitizes username, firstname, lastname, email, password. And adds the new user to the database.
   if ($_POST['password'] === $_POST['confirmPassword']) {
@@ -28,55 +29,7 @@ if(isset($_POST['email'], $_POST['password'], $_POST['username'], $_POST['firstN
     $statement->bindParam(':firstName', $firstName, PDO::PARAM_STR);
     $statement->bindParam(':lastName', $lastName, PDO::PARAM_STR);
     $statement->execute();
-}
-}
-//
-//
-// //Checks if username, email, name, password exists in the POST request:
-// if (isset($_POST['username'], $_POST['email'], $_POST['firstName'], $_POST['lastName'], $_POST['password'], $_POST['confirmPassword'])) {
-//
-//
-//   //Checks if the password match & sanitizes username, firstname, lastname, email, password. And adds the new user to the database.
-//   if ($_POST['password'] === $_POST['confirmPassword']) {
-//
-//       $firstName = trim(filter_var($_POST['firstName'], FILTER_SANITIZE_STRING));
-//       $lastName = trim(filter_var($_POST['lastName'], FILTER_SANITIZE_STRING));
-//       $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
-//       $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
-//       $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-//       // $created_at = date("Y-m-d");
-//
-//       $statement = $pdo->prepare('INSERT INTO Users(first_name, last_name, email, username, password)
-//        VALUES (:firstName, :lastName, :email, :username, :password)');
-//
-//         if (!$statement){
-//         die(var_dump($pdo->errorInfo()));
-//         }
-//
-//
-//         // //Checks if a user already exists:
-//         // if ($user['email'] == $email || $user['username'] == $username) {
-//         //   $statement = $pdo->prepare("SELECT email, username FROM Users WHERE username = :username AND email = :email");
-//         //   $_SESSION['message'] = 'You already have an account';
-//         //   redirect('/login.php');
-//         // }
-//         //
-//         // if (!$statement){
-//         // die(var_dump($pdo->errorInfo()));
-//         // }
-//
-//         //Binds variables to parameteres for insert statement:
-//         $statement->bindParam(':firstName', $firstName, PDO::PARAM_STR);
-//         $statement->bindParam(':lastName', $lastName, PDO::PARAM_STR);
-//         $statement->bindParam(':email', $email, PDO::PARAM_STR);
-//         $statement->bindParam(':username', $username, PDO::PARAM_STR);
-//         $statement->bindParam(':password', $password, PDO::PARAM_STR);
-//         // $statement->bindParam(':created_at', $created_at, PDO::PARAM_STR);
-//
-//         die(var_dump($statement));
-//
-//         $statement->execute();
 
-        redirect('/login.php');
-//   }
-// };
+    }
+}
+  redirect('/login.php');
