e2295268e043efd5ce07ef51d6a9eb1aef8e5e16
Isabelle Krall <isabellekrall@gmail.com>
2019-01-15 16:26:32 +0100

    updated $destination
    to updated folder

--- app/posts/posts.php
+++ app/posts/posts.php
@@ -2,36 +2,37 @@
 declare(strict_types=1);
 require __DIR__.'/../autoload.php';
 // In this file we store/insert new posts in the database.
-if(isset($_FILES['content'])) {
+if(isset($_FILES['content'], $_POST['username'])) {
     $content = $_FILES['content'];
+    $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
     $created_at = date("Y-m-d");
     $description = trim(filter_var($_POST['description'], FILTER_SANITIZE_STRING)) ?? '';
     $errors = [];
+
     if($content['size'] >= 4194304) {
         $errors[] = $content['name'] . ' Is too big';
     }
-    if (!file_exists(__DIR__ .'/../uploads/' . $_SESSION['user']['id'] . '/posts/')) {
-        mkdir(__DIR__ .'/../uploads/' . $_SESSION['user']['id'] . '/posts/', 0777, true);
+    if (!file_exists(__DIR__ .'/../uploads/')) {
+        mkdir(__DIR__ .'/../uploads/');
     }
     if(count($errors) > 0) {
         $_SESSION['errors'] = $errors;
         print_r($errors);
         exit;
     }
-    // Here we check if the user already have a folder for uploads, if not, create one.
-    if (!file_exists(__DIR__ .'/../uploads/' . $_SESSION['user']['id'] . '/posts/')) {
-        mkdir(__DIR__ .'/../uploads/' . $_SESSION['user']['id'] . '/posts/', 0777, true);
-    }
-    $destination = '/../uploads/' . $_SESSION['user']['id']  . '/posts/' . $content['name'];
+
+    $destination = '/../uploads/' . $content['name'];
     move_uploaded_file($content['tmp_name'], __DIR__.$destination);
-    // Set the $destination variable to be stored in the DB.
-    $destination = '/app/uploads/' . $_SESSION['user']['id'] . '/posts/' . time() . '-' . $content['name'];
-    $statement = $pdo->prepare('INSERT INTO Posts (user_id, description, content, created_at)
-    VALUES (:user_id, :description, :content, :created_at)');
+
+    //Sets destination and inserts file in DB
+    $destination = '/app/uploads/' . $content['name'];
+    $statement = $pdo->prepare('INSERT INTO Posts (user_id, description, content, created_at, username)
+    VALUES (:user_id, :description, :content, :created_at, :username)');
     $statement->bindParam(':user_id', $_SESSION['user']['id'], PDO::PARAM_INT);
     $statement->bindParam(':description', $description, PDO::PARAM_STR);
     $statement->bindParam(':content', $content['name'], PDO::PARAM_STR);
     $statement->bindParam(':created_at', $created_at, PDO::PARAM_STR);
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
     $statement->execute();
 
     if(!$statement){
