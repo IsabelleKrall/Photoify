fead2114fb1a16ab852db196f140c1c6463ef808
Isabelle Krall <isabellekrall@gmail.com>
2019-01-15 16:34:36 +0100

    cleaned up the code

--- app/users/biography.php
+++ app/users/biography.php
@@ -2,33 +2,35 @@
 declare(strict_types=1);
 require __DIR__.'/../autoload.php';
 
-if (isset($_POST['profile_bio'])) {
-    $id = $_SESSION['user']['id'];
-    $bio = filter_var($_POST['profile_bio'],FILTER_SANITIZE_STRING);
-    $statement = $pdo->prepare('UPDATE Users SET profile_bio = :profile_bio WHERE id = :id');
+//DELETE THIS FILE
 
-      if (!$statement){
-            die(var_dump($pdo->errorInfo()));
-      }
+// if (isset($_POST['profile_bio'])) {
+//     $id = $_SESSION['user']['id'];
+//     $bio = filter_var($_POST['profile_bio'],FILTER_SANITIZE_STRING);
+//     $statement = $pdo->prepare('UPDATE Users SET profile_bio = :profile_bio WHERE id = :id');
+//
+//       if (!$statement){
+//             die(var_dump($pdo->errorInfo()));
+//       }
 
       //Binds variables to parameteres for insert statement
-      $statement->bindParam(':profile_bio', $bio, PDO::PARAM_STR);
-      $statement->bindParam(':id', $id, PDO::PARAM_INT);
-      $statement->execute();
-      $statement = $pdo->prepare('SELECT * FROM Users WHERE id = :id');
-      $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_STR);
-      $statement->execute();
-      $user = $statement->fetch(PDO::FETCH_ASSOC);
-
-      $_SESSION['logedin'] = [
-          'id' => $user['id'],
-          'email' => $user['email'],
-          'name' => $user['name'],
-          'profile_pic' => $user['profile_pic'],
-          'profile_bio' => $user['profile_bio'],
-          'created_at' => $user['created_at'],
-          'username' => $user['username'],
-          ];
-
-redirect('/profile.php');
-};
+//       $statement->bindParam(':profile_bio', $bio, PDO::PARAM_STR);
+//       $statement->bindParam(':id', $id, PDO::PARAM_INT);
+//       $statement->execute();
+//       $statement = $pdo->prepare('SELECT * FROM Users WHERE id = :id');
+//       $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_STR);
+//       $statement->execute();
+//       $user = $statement->fetch(PDO::FETCH_ASSOC);
+//
+//       $_SESSION['logedin'] = [
+//           'id' => $user['id'],
+//           'email' => $user['email'],
+//           'name' => $user['name'],
+//           'profile_pic' => $user['profile_pic'],
+//           'profile_bio' => $user['profile_bio'],
+//           'created_at' => $user['created_at'],
+//           'username' => $user['username'],
+//           ];
+//
+// redirect('/profile.php');
+// };
